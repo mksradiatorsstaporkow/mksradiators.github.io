ce36d275eda56374b56ae870dc97b8ac8fa1d11f
mksradiatorsstaporkow <oskarmalkiewicz0311@gmail.com>
2026-07-30 12:47:22 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,50 @@
-console.log("MKS Radiator$ Stąporków - strona działa!");
+const matchDate = new Date("August 8, 2026 17:00:00").getTime();
+
+
+const timer = setInterval(function(){
+
+
+const now = new Date().getTime();
+
+
+const distance = matchDate - now;
+
+
+
+const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+
+const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+
+
+const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+
+
+const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+
+
+document.getElementById("countdown").innerHTML =
+
+"⏳ Do meczu pozostało: " +
+
+days + " dni " +
+
+hours + " godz. " +
+
+minutes + " min. " +
+
+seconds + " sek.";
+
+
+
+if(distance < 0){
+
+clearInterval(timer);
+
+document.getElementById("countdown").innerHTML =
+"Mecz trwa lub już się odbył ⚽";
+
+}
+
+
+},1000);
